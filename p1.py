723b847903f2b810b8714f3bca6c827112f5d10f
PrajwalGpy <prajwalgpy@gmail.com>
2026-02-04 08:35:44 +0530

    input small project

--- p1.py
+++ p1.py
@@ -85,3 +85,17 @@
 
 # value = num1 + num2
 # print(value)
+
+
+# - Create a distance converter converting Km to miles
+# - Take two inputs from user: Their first name and the distance in km
+# - Print: Greet user by name and show km, and mile values
+# - 1 mile is 1.609 kilometers
+# - hint: use correct types for calculating and print
+# - Did you capitalize the name
+
+name = input("Enter the your name : ")
+distence = float(input("Enter your distance in km : "))
+km_in_miles = distence/1.609
+print(f"your name is {name.capitalize()} and the miles you are in {round(km_in_miles,2)}")
+
