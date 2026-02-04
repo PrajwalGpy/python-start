dc86069f28bf897b14c0a79759551097ba8c0ed2
PrajwalGpy <prajwalgpy@gmail.com>
2026-02-04 07:48:22 +0530

    type casting

--- p1.py
+++ p1.py
@@ -1,5 +1,23 @@
-name = "Prajwal"
-age = 20
-year = 2026
+# name = "Prajwal"
+# age = 20
+# year = 2026
 
-print("hiii my name is " + name + " i am " + str(age) + " old " + " corrent year is " +  str(year)) 
+# print("hiii my name is " + name + " i am " + str(age) + " old " + " corrent year is " +  str(year)) 
+
+
+# type casting
+
+# print(type(8.90))
+# print(type("45"))
+# print(type(True))
+# print(type(5))
+
+# a = int(4.5)
+# b = int("5")
+# c = float(5)
+# d = float("5")
+# f = str(2.3)
+# g = str(4)
+# h = int(float("4.5"))
+
+# print([a,b,c,d,f,g,h])
