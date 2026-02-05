e4a87522e55f06968fb4e29fad098b986150e74e
PrajwalGpy <prajwalgpy@gmail.com>
2026-02-05 08:16:06 +0530

    function

--- p1.py
+++ p1.py
@@ -268,17 +268,30 @@
 #5. Show only the names who only appear in one of the lists
 #6. Create a new cars-list without duplicates
 
-friends = {'John','Michael','Terry','Eric','Graham'}
-my_friends = {'Reg','Loretta','Colin','John','Graham'}
-cars =['900','420','V70','911','996','V90','911','911','S','328','900']
+# friends = {'John','Michael','Terry','Eric','Graham'}
+# my_friends = {'Reg','Loretta','Colin','John','Graham'}
+# cars =['900','420','V70','911','996','V90','911','911','S','328','900']
 
-print('Eric' and 'John' in friends)
+# print('Eric' and 'John' in friends)
 
-print(friends.union(my_friends))
+# print(friends.union(my_friends))
 
-print(friends.intersection(my_friends))
+# print(friends.intersection(my_friends))
 
-print(friends.difference(my_friends))
+# print(friends.difference(my_friends))
 
-cars = set(cars)
-print(cars)
+# cars = set(cars)
+# print(cars)
+
+
+
+# def greeting(name,age=40):
+#     print(f"heee you are {name} and you are {str(age)} old")
+
+# name = input("Enter your name : ")
+# age = input("Enter your age : ")
+
+# if age:
+#     greeting(name,age)
+# else:
+#     greeting(name)
