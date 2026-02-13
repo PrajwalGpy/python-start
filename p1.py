5440e3ef91bfc6455791a823a436d2fc61963c5c
PrajwalGpy <prajwalgpy@gmail.com>
2026-02-13 07:41:23 +0530

    class project

--- p1.py
+++ p1.py
@@ -827,3 +827,53 @@
 
 # char3.magic()
 # char3.heal()  
+
+
+#  🍕 Pizza Builder — Challenge Steps
+#
+# 1. Define a Pizza class that stores:
+#    - size, crust type, and a list of toppings
+# 2. Add a method to add a new topping
+# 3. Add a method to remove a topping if it exists
+# 4. Add a method to print pizza details:
+#    - size, crust, and all toppings (or “No toppings yet!”)
+# 5. Create a pizza object, customize it, and print the summary
+
+# class Pizza:
+    
+#     def __init__(self,size,crust_type,list_of_toppings=None):
+#         self.size = size
+#         self.crust_type = crust_type
+#         self.list_of_toppings = list_of_toppings if list_of_toppings else []
+    
+    
+#     def add_new_topping(self,new_toppings):
+#         if isinstance(new_toppings,list):
+#             self.list_of_toppings.extend(new_toppings)
+#         else:
+#             self.list_of_toppings.append(new_toppings)
+
+    
+#     def remove_toppings(self,toppings):
+#         if toppings in self.list_of_toppings :
+#             self.list_of_toppings.remove(toppings)
+#         else:
+#             print(f"{toppings} didn't exits")
+    
+#     def display(self):
+#         print("🍕 --------Your Pizza Details--------------")
+#         print(f"Size : {self.size.title()}")
+#         print(f"crust : {self.crust_type.title()}")
+#         if self.list_of_toppings :
+#             print("Toppings")
+#             for num,toppings in enumerate(self.list_of_toppings,1):
+#                 print(f"{num}. {toppings.title()}")
+#         else:
+#             print("No Toppings")
+
+# myPizza = Pizza("small","deep")
+# myPizza.add_new_topping(["onions","salad"])
+# myPizza.add_new_topping("berry")
+# myPizza.remove_toppings("salad")
+# myPizza.remove_toppings("pajju")
+# myPizza.display()
