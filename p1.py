7c18d93ea7cd8157e9b59f884a59198ed37085d9
PrajwalGpy <prajwalgpy@gmail.com>
2026-02-14 07:16:40 +0530

    lambda 2

--- p1.py
+++ p1.py
@@ -900,3 +900,17 @@ print('Lambdas Exercise')
 # def f(x): return x + 5
 # f = lambda x,y: x+y
 # print(f(2,4))
+
+# def strip_spaces(str):
+#    return ''.join(str.split(' '))
+# #write equivalent lambda and insert Lambda here
+# strip_spaces1 = lambda str : ''.join(str.split(' '))
+# print(strip_spaces1('Monty Pythons Flying Circus')) 
+
+# def join_list_no_duplicates(list_a,list_b):
+#    return list(set(list_a + list_b))
+# list_a = [1,2,3,4]
+# list_b = [3,4,5,6,7]
+# #write lambda below 
+# join_list_no_duplicates1 = lambda list_a,list_b : list(set(list_a + list_b))
+# print(join_list_no_duplicates1(list_a,list_b))
