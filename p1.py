14d3a6ac1f72e78d7260c8db9d2db2d37135ccb0
Prajwal Poojary <136236335+PrajwalGpy@users.noreply.github.com>
2026-07-10 19:41:47 +0530

    Update p1.py

--- p1.py
+++ p1.py
@@ -5,7 +5,7 @@
 # print("hiii my name is " + name + " i am " + str(age) + " old " + " corrent year is " +  str(year)) 
 
 
-# type casting hooo yoiiii
+# type casting hooo yoiiii jjjjjjj
 
 # print(type(8.90))
 # print(type("45"))
